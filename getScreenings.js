878e75041b00518f67cf620d0a66d445878d6322
Hope <hkim@oscars.org>
2018-08-07 11:30:47 -0700

    Added a testdata.json file to use as a model. Refactoring getScreenings to send all rows not just the one that was updated.

--- getScreenings.js
+++ getScreenings.js
@@ -1,39 +1,45 @@
-var apiUrl = "http://35.163.42.76:8080/submit/los_angeles";
+var apiUrl      = "http://35.163.42.76:8080/submit/los_angeles";
+var createEvent = "events/create/";
+var saveEvent   = "events/save/";
+var token       = "api1531940172LJDNgYHieIyvSu2ORGsx25545";
 
-var fields;
+var headers;
 var request, response;
 var activeSheet, editedRow;
 
 function main(e) {
-  fields         = getValues(e, 1);
+  headers        = getValues(e, 1);
   editedRow      = e.source.getActiveRange().getRow();
   var edits      = getValues(e, editedRow);
   var parameters = designData(edits);
 
-  sendScreening(parameters);
-  // updateScreeningIfNeeded();
+  sendEvent(parameters);
 }
 
 function getValues(e, r) {
   activeSheet = e.source.getActiveSheet();
-  var values  = activeSheet.getRange("A" + r + ":O" + r).getValues();
+  var numOfEvents;
+  var vs      = activeSheet.getRange("A" + r + ":O" + r).getValues();
 
-  return values;
+  console.log(e.source)
+  console.log("typeof values vs is", typeof vs)
+  console.log("values vs", vs)
+  return vs;
 }
 
 function designData(data) {
-  var screening = {};
+  var event = {};
   var eventInfo = data[0];
 
   eventInfo.map(function (d, i) {
-    screening[fields[0][i]] = encodeURIComponent(data[0][i])
+    event[headers[0][i]] = data[0][i]
   })
 
-  var dateTime            = convertDateTime(screening.Date, screening.Time, screening.Run_Time);
-  screening.startDateTime = dateTime.start;
-  screening.endDateTime   = dateTime.end;
+  var dateTime            = convertDateTime(event.Date, event.Time, event.Run_Time);
+  event.startDateTime = dateTime.start;
+  event.endDateTime   = dateTime.end;
 
-  return screening;
+  return event;
 }
 
 function convertDateTime(date, time, runTime) {
@@ -53,12 +59,13 @@ function convertDateTime(date, time, runTime) {
   // need to add the 0 for the seconds
   var formattedDateTime = new Date(dateValueObj.year, dateValueObj.month, dateValueObj.day, timeValueObj.hour, timeValueObj.minutes, 0);
   var startDateTimeMs   = formattedDateTime.getTime();
-  var startDateTime     = Utilities.formatDate(formattedDateTime, "PST", "MM/dd/yyyy kk:mm")
+  var startDateTime     = Utilities.formatDate(formattedDateTime, "PST", "yyyy-MM-dd'T'HH:mm:ss'Z'")
 
   var endDateTimeMs        = startDateTimeMs + (runTime * 60000);
+  console.log(startDateTimeMs, endDateTimeMs)
   // not totally sure why you have to add the (+) operator but you do
   var formattedEndDateTime = new Date(+endDateTimeMs);
-  var endDateTime          = Utilities.formatDate(formattedEndDateTime, "PST", "MM/dd/yyyy kk:mm");
+  var endDateTime          = Utilities.formatDate(formattedEndDateTime, "PST", "yyyy-MM-dd'T'HH:mm:ss'Z'");
 
   return {
     start: startDateTime,
@@ -66,23 +73,24 @@ function convertDateTime(date, time, runTime) {
   }
 }
 
-function sendScreening(p) {
+function sendEvent(payload) {
+  //var calendarId = 153194017366433;
   //var eventDoesntExist = p.ID === "" || p.ID === undefined || p.ID === typeof 'undefined'
   //console.log("eventDoesntExist:", eventDoesntExist)
-  //var methodType = eventDoesntExist ? createEvent : saveEvent;
+  //var methodType    = eventDoesntExist ? createEvent : saveEvent;
 
-  var payload = {
-    title: p.Film_Title,
-    synopsis: p.Synopsis
+  // var postUrl = apiUrl + methodType + "?token=" + token + "&calendar_id=" + calendarId + "&title=" + p.Film_Title + "&description=" + p.Synopsis + "&location=" + p.Venue_Name + " " + p.Venue_Address + "&timezone=America/Los_Angeles" + "&start_date=" + p.startDateTime + "&end_date=" + p.endDateTime;
+
+  request = function () {
+    return response = UrlFetchApp.fetch(apiUrl, {
+      "method": "post",
+      "body": payload
+    });
   }
 
-  (function () {
-    return request = UrlFetchApp.fetch(apiUrl, {
-      "method": "post",
-      "body": JSON.stringify(payload)
-    });
-  })()
+  console.log(payload)
 
+  request();
 }
 
 // function checkEvent(){
